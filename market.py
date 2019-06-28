9e17a4a047488e2203bd98877f90cf68fc907f49
Ali <guoxin@xkw.cn>
2019-06-28 09:48:49 +0800

    octopus.conf 采用 json 格式

--- market.py
+++ market.py
@@ -6,7 +6,7 @@ import os
 import threading
 import time
 import memcache
-import configparser
+import json
 
 mc = memcache.Client(['127.0.0.1:11211'], debug=True)
 
@@ -21,54 +21,46 @@ from octopus.exchange.exchange import exchange as ex
 def set_config(conf_path):
 
     try:
-        cf = configparser.ConfigParser()
-        cf.read(conf_path, encoding='utf-8')
 
-        '''
-        全局币种设置
-        适用于交易所内币种是否设置准确的检测
-        '''
-        symbols = []
-        for opt in cf.options('symbols'):
-            for symbol in cf.get('symbols', opt).split(','):
-                symbols.append("%s_%s" % (symbol, opt))
-        ex.symbols = symbols
+        with open(conf_path, 'r') as f:
+            cf = json.load(f)
 
-        '''
-        交易所内币种设置
-        超出全局币种设置的将被忽略 不会进行深度及空间的检测 更不会进行搬砖
-        '''
-        for st in cf.sections():
-            data = {}
-            if '.symbols' in st:
-                e = st.split('.')[0]
-                for opt in cf.options(st):
-                    data[opt] = cf.get(st, opt)
-                ex.exchanges[e].symbols = data
+            for c in ['symbols', 'apiconfig']:
+                if c not in cf:
+                    print("config no %s!" % c)
+                    return False
 
-        '''
-        交易所基础API设置
-        仅用于市场深度以及空间的检测, 不适用于搬砖
-        搬砖所使用的 API 需要在 robot 的策略配置中传入
-        '''
-        for st in cf.sections():
-            data = {}
-            if '.apiconfig' in st:
-                e = st.split('.')[0]
-                for opt in cf.options(st):
-                    data[opt] = cf.get(st, opt)
-                ex.exchanges[e].apiconfig = data
+            '''
+            全局币种设置
+            适用于交易所内币种是否设置准确的检测
+            '''
+            ex.symbols = cf.get("symbols").get("global", [])
 
+            for e in ex.exchanges:
+
+                '''
+                交易所内币种设置
+                超出全局币种设置的将被忽略 不会进行深度及空间的检测 更不会进行搬砖
+                '''
+                ex.exchanges[e].symbols = cf.get("symbols").get(e, {})
+
+                '''
+                交易所基础API设置
+                仅用于市场深度以及空间的检测, 不适用于搬砖
+                搬砖所使用的 API 需要在 robot 的策略配置中传入
+                '''
+                ex.exchanges[e].apiconfig = cf.get("apiconfig").get(e, {})
 
         return True
 
-    except:
+    except Exception as e:
+        print(e)
         return False
 
 """
 市场深度
 1. 实时查询各个交易所各个币种的挂单价格
-2. 将挂单数据存储在 memcache 数据库中、数据存活期为 3 秒钟
+2. 将挂单数据存储在 memcache 数据库中、数据存活期为 5 秒钟
 3. 提供某个市场某个币种交易深度的订阅接口
 """
 class depth:
