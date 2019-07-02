9a5756fe85d5344b14a6af889e86b76372d8ac91
Ali <guoxin@xkw.cn>
2019-07-02 23:42:35 +0800

    test - Single Exchange Grid Trading

--- engine/segt.py
+++ engine/segt.py
@@ -5,30 +5,80 @@
 Single Exchange Grid Trading
 单交易所网格交易
 '''
+import os, sys, time, threading, json
 
-import sys
-import os
-import time
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# print(root_path)
+sys.path.append(root_path)
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from octopus.exchange.exchange import exchange as ex
+from octopus.market import depth, space, account
 
-from utils.db import db
-from utils.ex import ex
-from cron.otc import otc
-from config import config
-
-
-def segt(exchange, symbol, i):
-    depth = db.redis.hgetall('depth.%s.%s.%s' % (exchange, symbol, i))
-
-    if depth:
-        pass
+def format_price(price, fluctuate):
+    for n in range(-10, 10):
+        if int(fluctuate * (10 ** n)) > 0:
+            break
+    return round(float(int(price * (10 ** n))) / (10 ** n), n)
 
 
-while True:
-    print(time.strftime("%Y-%m-%d %H:%M:%S"), '========== SEGT ==========')
+def segt(conf_path):
 
-    for exchange, symbols in ex.list:
-        for symbol in symbols:
-            for i in range(config.depth_size):
-                segt(exchange, symbol, i)
+    print(conf_path)
+
+    try:
+        with open(conf_path, 'r') as f:
+            cf = json.load(f)
+
+            for c in ['exchange', 'apiconfig', 'symbol', 'fluctuate', 'amount']:
+                if c not in cf:
+                    print("config no %s!" % c)
+                    return
+
+            exchange = cf.get("exchange")
+            apiconfig = cf.get("apiconfig")
+            symbol = cf.get("symbol")
+            fluctuate = cf.get("fluctuate")
+
+            _low, _high = 0.0, 0.0
+
+            while True:
+
+                data = depth.get(exchange , symbol)
+
+                if data:
+
+                    bp = float(data['buy_price'])
+                    sp = float(data['sell_price'])
+
+                    low = format_price(min(bp, sp), fluctuate)
+                    high = format_price(max(bp, sp), fluctuate)
+
+                    action = "."
+
+                    if low > _low and _low > 0.0:
+                        action = "[ buy %f ]" % (low - fluctuate)
+                    if low < _low and _low > 0.0:
+                        action = "[ sell %f ]" % (low + fluctuate)
+                    if high > _high and _high > 0.0:
+                        action = "[ buy %f ]" % (high - fluctuate)
+                    if high < _high and _high > 0.0:
+                        action = "[ sell %f ]" % (high + fluctuate)
+
+                    print(time.strftime("%Y-%m-%d %H:%M:%S"), data, 'low:', low, 'high:', high, action)
+
+                    _low = low
+                    _high = high
+
+                time.sleep(1)
+
+    except Exception as e:
+        print(e.message)
+        print("error config!")
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 2:
+        conf_path = sys.argv[1]
+        threading.Thread(target=segt, args=(conf_path,)).start()
+    else:
+        print("usage: python segt.py segt.conf")
